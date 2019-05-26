70fe194456559ba57e48a61cc1c51426cb7fffd2
jaswanthsurya <jassusai0611@gmail.com>
2019-05-26 19:48:54 +0530

    added controller for populating dropdown list

---
+++ Glimpse/Controllers/PopulateDDLController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using ReportingService.Dtos;
+
+namespace ReportingService.Controllers
+{
+    public class PopulateDDLController : ApiController
+    {
+        public HttpResponseMessage Get(string SpName)
+        {
+            try
+            {
+                var cs = ConfigurationManager.ConnectionStrings["DBCS"].ToString();
+                SqlDataReader rdr;
+                var queryResult = new List<DdlDTO>();
+                int counter = 1;
+                using (var con = new SqlConnection(cs))
+                {
+                    using (var cmd = new SqlCommand(SpName, con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        con.Open();
+                        rdr = cmd.ExecuteReader();
+                        while (rdr.Read())
+                        {
+                            queryResult.Add(new DdlDTO
+                            {
+                                value = rdr["dname"].ToString(),
+                                index = counter++
+                            });
+                        }
+                    }
+                }
+                return Request.CreateResponse(HttpStatusCode.OK,queryResult);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.ServiceUnavailable,"INTERNAL SERVER PROBLEM");
+            }
+        }
+    }
+}
---
+++ Glimpse/Dtos/DdlDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReportingService.Dtos
+{
+    public class DdlDTO
+    {
+        public string value { get; set; }
+        public int index { get; set; }
+    }
+}
